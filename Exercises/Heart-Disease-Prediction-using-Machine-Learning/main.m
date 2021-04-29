5cff23b93def2d1c9d87b1b1d367830f14ddf168
yiqiangjizhang <yiqiangjizhang@gmail.com>
2021-04-29 18:26:41 +0200

    Added Report Heart Disease using Machine Learning

--- Exercises/Heart-Disease-Prediction-using-Machine-Learning/main.m
+++ Exercises/Heart-Disease-Prediction-using-Machine-Learning/main.m
@@ -1,14 +1,22 @@
 %% Machine Learning
 %
 %-------------------------------------------------------------------------%
-% Airfoil Self-Noise Data Set
+% Heart Disease Data Set
 %
-% NASA data set, obtained from a series of aerodynamic and acoustic tests of two and three-dimensional airfoil blade sections conducted in an anechoic wind tunnel.
-% URL: https://archive.ics.uci.edu/ml/datasets/Airfoil+Self-Noise#
-% 
+% This database contains 76 attributes, but all published experiments refer to using 
+% a subset of 14 of them. In particular, the Cleveland database is the only one that 
+% has been used by ML researchers to this date. 
+%
+% The "goal" field refers to the presence of heart disease in the patient. 
+% It is integer valued from 0 (no presence) to 4. Experiments with the Cleveland 
+% database have concentrated on simply attempting to distinguish presence 
+% (values 1,2,3,4) from absence (value 0).
+%
+% https://archive.ics.uci.edu/ml/datasets/Heart+Disease
+% url: https://www.kaggle.com/ronitf/heart-disease-uci
 %-------------------------------------------------------------------------%
 
-% Date: 23/04/2021
+% Date: 29/04/2021
 % Author/s: Yi Qiang Ji Zhang
 % Subject: NUMERICAL TOOLS IN MACHINE LEARNING FOR AERONAUTICAL ENGINEERING
 % Professor: Alex Ferrer Ferre
@@ -25,24 +33,103 @@ set(groot,'defaultLegendInterpreter','latex');
 
 %% Analyse data
 
-% Load data
-load airfoilselfnoise
+fine_tree = 73.3;
+medium_tree = 73.3;
+coarse_tree = 80.0;
+linear_discriminant = 82.7;
+quadratic_discriminant = 81.3;
+logistic_regression = 84.0;
+naive_bayes_gaussian = 86.7;
+naive_bayes_kernel = 80.0;
+SVM_linear = 84.0;
+SVM_quadratic = 85.3;
+SVM_cubic = 72.0;
+SVM_fine_gaussian = 58.7;
+SVM_medium_gaussian = 84.0;
+SVM_coarse_gaussian = 82.7;
+KNN_fine = 76.0;
+KNN_medium = 85.3;
+KNN_coarse = 84.0;
+KNN_cosine = 82.7;
+KNN_cuibc = 81.3;
+KNN_weighted = 86.7;
+Ensemble_boosted_trees = 78.7;
+Ensemble_bagged_trees = 86.7;
+Ensemble_subspace_discriminant = 84.0;
+Ensemble_subspace_KNN = 70.7;
+Ensemble_RUSBoosted_trees = 81.3;
 
-% Read data
-% 1. Frequency, in Hertzs.
-frequency = airfoilselfnoise(:,1);
-% 2. Angle of attack, in degrees.
-angle_of_attack = airfoilselfnoise(:,2);
-% 3. Chord length, in meters.
-chord_length = airfoilselfnoise(:,3);
-% 4. Free-stream velocity, in meters per second.
-free_stream_velocity = airfoilselfnoise(:,4);
-% 5. Suction side displacement thickness, in meters.
-suction_side_displ = airfoilselfnoise(:,5);
-% 6. Scaled sound pressure level, in decibels.
-scaled_sound_pressure = airfoilselfnoise(:,6);
+x =  categorical({'fine tree', 'medium tree', 'coarse tree', 'linear discriminant', ...
+    'quadratic discriminant', 'logistic regression', 'naive bayes gaussian', ...
+    'naive bayes kernel', 'SVM linear', 'SVM quadratic', 'SVM cubic', ...
+    'SVM fine gaussian', 'SVM medium gaussian', 'SVM coarse gaussian', ...
+    'KNN fine', 'KNN medium', 'KNN coarse', 'KNN cosine', 'KNN cuibc', ...
+    'KNN weighted', 'Ensemble boosted trees', 'Ensemble bagged trees', ...
+    'Ensemble subspace discriminant', 'Ensemble subspace KNN', ...
+    'Ensemble RUSBoosted trees'});
+
+y = [fine_tree;
+medium_tree;
+coarse_tree;
+linear_discriminant;
+quadratic_discriminant;
+logistic_regression;
+naive_bayes_gaussian;
+naive_bayes_kernel;
+SVM_linear;
+SVM_quadratic;
+SVM_cubic;
+SVM_fine_gaussian;
+SVM_medium_gaussian;
+SVM_coarse_gaussian;
+KNN_fine;
+KNN_medium;
+KNN_coarse;
+KNN_cosine;
+KNN_cuibc;
+KNN_weighted;
+Ensemble_boosted_trees;
+Ensemble_bagged_trees;
+Ensemble_subspace_discriminant;
+Ensemble_subspace_KNN;
+Ensemble_RUSBoosted_trees]';
+
+% Plot
+plot1 = figure(1);
+bar(x,y)
+% text(1:length(y),y,num2str(y'),'HorizontalAlignment','center','VerticalAlignment','bottom'); 
+set(plot1,'Position',[475 150 1000 800])
+ylabel('Accuracy (%)')
+title('Plot')
+box on
+grid minor
+hold off;
+
+% % Save pdf
+% set(plot1, 'Units', 'Centimeters');
+% pos = get(plot1, 'Position');
+% set(plot1, 'PaperPositionMode', 'Auto', 'PaperUnits', 'Centimeters', ...
+%     'PaperSize',[pos(3), pos(4)]);
+% print(plot1, 'accuracy.pdf', '-dpdf', '-r0');
+% 
+% % Save png
+% print(gcf,'accuracy.png','-dpng','-r100');
 
 
-airfoil_self_noise = table(frequency, angle_of_attack, chord_length, ...
-    free_stream_velocity, suction_side_displ, scaled_sound_pressure);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ Exercises/Heart-Disease-Prediction-using-Machine-Learning/test.m
@@ -0,0 +1,4 @@
+Y=[198 138 172 188 190 192];
+bar(Y);
+text(1:length(Y),Y,num2str(Y'),'vert','bottom','horiz','center'); 
+box off
